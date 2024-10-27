aaea31ec51c41fcaabd390bcad9e43addf572df9
Mokete <emtyabekana@gmail.com>
2024-10-27 18:54:09 +0200

    Created a function to fetch data randomly, and inside it I added try and catch to fetch data from the API and display an error message if the we cant get data from the API.

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 const apiKey='f16246695ef64c608bd34c91f3ee6bb6';
+
+const blogContainer=document.getElementById('news-content');
+
+function fetchRandomNews(){
+    try{
+
+    }catch(error){
+console.error('Error Fetching The News Data!')
+    }
+}

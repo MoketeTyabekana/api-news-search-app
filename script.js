b5e29aef36c7a93d271a76efbab78b47b388a839
Mokete <emtyabekana@gmail.com>
2024-10-27 19:17:52 +0200

    Used async/await to add some delay on our data fetching process.

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ const apiKey='f16246695ef64c608bd34c91f3ee6bb6';
 
 const blogContainer=document.getElementById('news-content');
 
-function fetchRandomNews(){
+async function fetchRandomNews(){
     try{  
         const apiUrl='https://newsapi.org/v2/top-headlines?country=us&pageSize=10&apiKey=${apiKey}';
+
+        const response =await fetch (apiUrl);
 
     }catch(error){
 console.error('Error Fetching random news data!', error);
